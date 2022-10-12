76a03da348d20a1113c9a9803e897f1377e62e8e
Jack Ren <bjrjk@qq.com>
2022-10-12 11:52:35 +0800

    Completed Heap implementation

--- Assignment1/ast-interpreter/Environment.h
+++ Assignment1/ast-interpreter/Environment.h
@@ -1,6 +1,14 @@
 //==--- tools/clang-check/ClangInterpreter.cpp - Clang Interpreter tool --------------===//
 //===----------------------------------------------------------------------===//
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <cctype>
+#include <vector>
+#include <algorithm>
+#include <unistd.h>
+
+using namespace std;
 
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/Decl.h"
@@ -10,6 +18,8 @@
 #include "clang/Tooling/Tooling.h"
 
 using namespace clang;
+
+typedef unsigned int uint_t;
 
 class StackFrame {
     /// StackFrame maps Variable Declaration to Value
@@ -50,18 +60,107 @@ public:
 };
 
 /// Heap maps address to a value
-/*
+
 class Heap {
+private:
+    struct chunkMeta {
+    private:
+        uint_t begin; // in bytes
+        uint_t length, capacity; // in bytes
+        char *pointer; // real pointer in host
+    public:
+        chunkMeta(uint_t length, uint_t &addressAccumulator) : length(length) {
+            this->begin = addressAccumulator;
+            this->capacity = (length + 7) & 0xfffffff8; // align chunk to 8 bytes
+            addressAccumulator += capacity;
+            this->pointer = static_cast<char *>(malloc(this->capacity));
+        }
+
+        ~chunkMeta() {
+            free(this->pointer);
+            this->pointer = nullptr;
+        }
+
+        uint_t getBegin() const { return begin; }
+
+        uint_t getLength() const { return length; }
+
+        int get(uint_t byteOffset) const {
+            assert(byteOffset < length && byteOffset < capacity);
+            int *intElemPtr = reinterpret_cast<int *>(pointer + byteOffset);
+            return *intElemPtr;
+        }
+
+        void set(uint_t byteOffset, int value) {
+            assert(byteOffset < length && byteOffset < capacity);
+            int *intElemPtr = reinterpret_cast<int *>(pointer + byteOffset);
+            *intElemPtr = value;
+        }
+    };
+
+    unsigned int addressAccumulator;
+    vector<chunkMeta *> chunks;
+
+    chunkMeta* queryChunkMeta(int addr) const {
+        auto findIter = find_if(chunks.begin(), chunks.end(), [&](chunkMeta *chunk) {
+            uint_t begin = chunk->getBegin(), length = chunk->getLength();
+            uint_t end = begin + length;
+            return begin <= addr && addr < end;
+        });
+        assert(findIter != chunks.end());
+        if (findIter != chunks.end()) {
+            return *findIter;
+        } else {
+            return nullptr;
+        }
+    }
 public:
-   int Malloc(int size) ;
-   void Free (int addr) ;
-   void Update(int addr, int val) ;
-   int get(int addr);
+    Heap() : addressAccumulator(0) {}
+    ~Heap() {
+        for_each(chunks.begin(), chunks.end(), [](chunkMeta *&chunk) {
+            delete chunk;
+            chunk = nullptr;
+        });
+        chunks.clear();
+    }
+
+    int allocate(int size) {
+        chunkMeta *chunk = new chunkMeta(size, addressAccumulator);
+        chunks.push_back(chunk);
+        return chunk->getBegin();
+    }
+
+    void release(int addr) {
+        // Remove only when `addr` is chunk's begin address
+        remove_if(chunks.begin(), chunks.end(), [&](chunkMeta *&chunk) {
+            if (chunk->getBegin() == addr) {
+                delete chunk;
+                chunk = nullptr;
+                return true;
+            }
+            return false;
+        });
+    }
+
+    void set(int addr, int val) {
+        chunkMeta *chunk = queryChunkMeta(addr);
+        uint_t byteOffset = addr - chunk->getBegin();
+        assert(byteOffset < chunk->getLength());
+        chunk->set(byteOffset, val);
+    }
+
+    int get(int addr) const {
+        chunkMeta *chunk = queryChunkMeta(addr);
+        uint_t byteOffset = addr - chunk->getBegin();
+        assert(byteOffset < chunk->getLength());
+        return chunk->get(byteOffset);
+    }
 };
-*/
+
 
 class Environment {
-    std::vector <StackFrame> mStack;
+    std::vector<StackFrame> mStack;
+    Heap heap;
 
     FunctionDecl *mFree;                /// Declartions to the built-in functions
     FunctionDecl *mMalloc;
@@ -78,7 +177,7 @@ public:
     /// Initialize the Environment
     void init(TranslationUnitDecl *unit) {
         for (TranslationUnitDecl::decl_iterator i = unit->decls_begin(), e = unit->decls_end(); i != e; ++i) {
-            if (FunctionDecl * fdecl = dyn_cast<FunctionDecl>(*i)) {
+            if (FunctionDecl *fdecl = dyn_cast<FunctionDecl>(*i)) {
                 if (fdecl->getName().equals("FREE")) mFree = fdecl;
                 else if (fdecl->getName().equals("MALLOC")) mMalloc = fdecl;
                 else if (fdecl->getName().equals("GET")) mInput = fdecl;
@@ -101,7 +200,7 @@ public:
         if (bop->isAssignmentOp()) {
             int val = mStack.back().getStmtVal(right);
             mStack.back().bindStmt(left, val);
-            if (DeclRefExpr * declexpr = dyn_cast<DeclRefExpr>(left)) {
+            if (DeclRefExpr *declexpr = dyn_cast<DeclRefExpr>(left)) {
                 Decl *decl = declexpr->getFoundDecl();
                 mStack.back().bindDecl(decl, val);
             }
@@ -112,7 +211,7 @@ public:
         for (DeclStmt::decl_iterator it = declstmt->decl_begin(), ie = declstmt->decl_end();
              it != ie; ++it) {
             Decl *decl = *it;
-            if (VarDecl * vardecl = dyn_cast<VarDecl>(decl)) {
+            if (VarDecl *vardecl = dyn_cast<VarDecl>(decl)) {
                 mStack.back().bindDecl(vardecl, 0);
             }
         }
@@ -140,23 +239,31 @@ public:
     /// !TODO Support Function Call
     void call(CallExpr *callexpr) {
         mStack.back().setPC(callexpr);
-        int val = 0;
         FunctionDecl *callee = callexpr->getDirectCallee();
         if (callee == mInput) {
+            int val;
 #ifndef ASSIGNMENT_DEBUG
             llvm::errs() << "Please Input an Integer Value : ";
 #endif
             scanf("%d", &val);
-
             mStack.back().bindStmt(callexpr, val);
         } else if (callee == mOutput) {
-            Expr *decl = callexpr->getArg(0);
-            val = mStack.back().getStmtVal(decl);
+            Expr *outputExp = callexpr->getArg(0);
+            int val = mStack.back().getStmtVal(outputExp);
 #ifndef ASSIGNMENT_DEBUG
             llvm::errs() << val;
 #else
             printf("%d\n", val);
 #endif
+        } else if (callee == mMalloc) {
+            Expr *chunkSizeExp = callexpr->getArg(0);
+            int chunkSize = mStack.back().getStmtVal(chunkSizeExp);
+            int chunkVMAddr = heap.allocate(chunkSize);
+            mStack.back().bindStmt(callexpr, chunkVMAddr);
+        } else if (callee == mFree) {
+            Expr *chunkVMAddrExp = callexpr->getArg(0);
+            int chunkVMAddr = mStack.back().getStmtVal(chunkVMAddrExp);
+            heap.release(chunkVMAddr);
         } else {
             /// You could add your code here for Function call Return
         }
