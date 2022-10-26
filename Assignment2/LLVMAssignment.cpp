3e0503409a2af967f565ff88e4e9781c43465650
Jack Ren <bjrjk@qq.com>
2022-10-26 18:39:30 +0800

    Judge preparation for assignment 2

--- Assignment2/LLVMAssignment.cpp
+++ Assignment2/LLVMAssignment.cpp
@@ -1,17 +1,3 @@
-//===- Hello.cpp - Example code from "Writing an LLVM Pass" ---------------===//
-//
-//                     The LLVM Compiler Infrastructure
-//
-// This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
-//
-//===----------------------------------------------------------------------===//
-//
-// This file implements two versions of the LLVM "Hello World" pass described
-// in docs/WritingAnLLVMPass.html
-//
-//===----------------------------------------------------------------------===//
-
 #include <llvm/Support/CommandLine.h>
 #include <llvm/IRReader/IRReader.h>
 #include <llvm/IR/LLVMContext.h>
@@ -32,76 +18,74 @@
 
 using namespace llvm;
 static ManagedStatic<LLVMContext> GlobalContext;
+
 static LLVMContext &getGlobalContext() { return *GlobalContext; }
+
 /* In LLVM 5.0, when  -O0 passed to clang , the functions generated with clang will
  * have optnone attribute which would lead to some transform passes disabled, like mem2reg.
  */
-struct EnableFunctionOptPass: public FunctionPass {
+struct EnableFunctionOptPass : public FunctionPass {
     static char ID;
-    EnableFunctionOptPass():FunctionPass(ID){}
-    bool runOnFunction(Function & F) override{
-        if(F.hasFnAttribute(Attribute::OptimizeNone))
-        {
+
+    EnableFunctionOptPass() : FunctionPass(ID) {}
+
+    bool runOnFunction(Function &F) override {
+        if (F.hasFnAttribute(Attribute::OptimizeNone)) {
             F.removeFnAttr(Attribute::OptimizeNone);
         }
         return true;
     }
 };
 
-char EnableFunctionOptPass::ID=0;
+char EnableFunctionOptPass::ID = 0;
 
-	
-///!TODO TO BE COMPLETED BY YOU FOR ASSIGNMENT 2
-///Updated 11/10/2017 by fargo: make all functions
-///processed by mem2reg before this pass.
 struct FuncPtrPass : public ModulePass {
-  static char ID; // Pass identification, replacement for typeid
-  FuncPtrPass() : ModulePass(ID) {}
+    static char ID; // Pass identification, replacement for typeid
+    FuncPtrPass() : ModulePass(ID) {}
 
-  
-  bool runOnModule(Module &M) override {
-    errs() << "Hello: ";
-    errs().write_escaped(M.getName()) << '\n';
-    M.dump();
-    errs()<<"------------------------------\n";
-    return false;
-  }
+
+    bool runOnModule(Module &M) override {
+        errs() << "Hello: ";
+        errs().write_escaped(M.getName()) << '\n';
+        M.dump();
+        errs() << "------------------------------\n";
+        return false;
+    }
 };
-
 
 char FuncPtrPass::ID = 0;
 static RegisterPass<FuncPtrPass> X("funcptrpass", "Print function call instruction");
 
 static cl::opt<std::string>
-InputFilename(cl::Positional,
-              cl::desc("<filename>.bc"),
-              cl::init(""));
+        InputFilename(cl::Positional,
+                      cl::desc("<filename>.bc"),
+                      cl::init(""));
 
 
 int main(int argc, char **argv) {
-   LLVMContext &Context = getGlobalContext();
-   SMDiagnostic Err;
-   // Parse the command line to read the Inputfilename
-   cl::ParseCommandLineOptions(argc, argv,
-                              "FuncPtrPass \n My first LLVM too which does not do much.\n");
+    LLVMContext &Context = getGlobalContext();
+    SMDiagnostic Err;
+    // Parse the command line to read the Inputfilename
+    cl::ParseCommandLineOptions(argc, argv,
+                                "FuncPtrPass \n Analyse function invocations.\n");
 
 
-   // Load the input module
-   std::unique_ptr<Module> M = parseIRFile(InputFilename, Err, Context);
-   if (!M) {
-      Err.print(argv[0], errs());
-      return 1;
-   }
+    // Load the input module
+    std::unique_ptr<Module> M = parseIRFile(InputFilename, Err, Context);
+    if (!M) {
+        Err.print(argv[0], errs());
+        return 1;
+    }
 
-   llvm::legacy::PassManager Passes;
-   	
-   ///Remove functions' optnone attribute in LLVM5.0
-   Passes.add(new EnableFunctionOptPass());
-   ///Transform it to SSA
-   Passes.add(llvm::createPromoteMemoryToRegisterPass());
+    llvm::legacy::PassManager Passes;
 
-   /// Your pass to print Function and Call Instructions
-   Passes.add(new FuncPtrPass());
-   Passes.run(*M.get());
+    ///Remove functions' optnone attribute in LLVM5.0
+    Passes.add(new EnableFunctionOptPass());
+    ///Transform it to SSA
+    Passes.add(llvm::createPromoteMemoryToRegisterPass());
+
+    /// Your pass to print Function and Call Instructions
+    Passes.add(new FuncPtrPass());
+    Passes.run(*M.get());
 }
 
